02b1e0c439520a0a30dff5c0647185b20c7233ae
Marcus P S <marcus.ps+github@gmail.com>
2010-06-02 13:47:01 -0400

    Modified copyright notices on qip package

--- qip/inst/asym_hermitian_matrix.m
+++ qip/inst/asym_hermitian_matrix.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2010   Marcus P da Silva   <marcus.ps+github@gmail.com>
+## Copyright (C) 2010   Marcus P da Silva http://github.com/marcusps
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
